479fa87b3f16d4f6fc6862e063f4a81d2773446b
yashsinghal98 <68867021+yashsinghal98@users.noreply.github.com>
2020-11-07 18:51:40 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
-
 function handleServer(req, res) {
-  
+  if (req.url === "/welcome") {
+    res.write("Welcome to Dominos!");
+    res.setStatus(200);
+    res.send();
+  } else if (req.url === "/contact") {
+    res.write({ phone: "18602100000", email: "guestcaredominos@jublfood.com" });
+    res.setStatus(200);
+    res.send();
+  } else {
+    res.setStatus(404);
+    res.send();
+  }
 }
-
+httpServer.listen(8081);
 module.exports = httpServer;
